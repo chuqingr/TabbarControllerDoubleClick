1f183f08e2f1a6035bba96d3574242cb22249ba9
chuqingr <953553352@qq.com>
2016-06-23 13:07:45 +0800

    Initial Commit

---
+++ TabbarControllerDoubleClick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbarControllerDoubleClick
+//
+//  Created by 杨胜浩 on 16/6/23.
+//  Copyright © 2016年 chuqingr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
